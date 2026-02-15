1f2a7755d7807ab381654a45a3a67e47e2876c3c
Chanel Chinedu <LongLiveTheKing777@protonmail.com>
2026-02-15 14:58:48 +0000

    space-around in progress

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ function displayTheme(event) {
 
 let openThemeSound = new Audio("../audios/route-101.mp3");
 let toggle = true;
+
 function displaySound() {
   let volumeImg = document.querySelector("#sound");
 
@@ -44,6 +45,42 @@ function pikaPika(event) {
   }
 }
 
+function displayPokemon() {
+  let pokemonElement = document.querySelector("#pokemon");
+  //let selectedPokemon = document.querySelector("#selected-pokemon");
+  if (pokemonElement) {
+    let selectedPokemon = document.querySelector("#selected-pokemon");
+    selectedPokemon.innerHTML = `
+      <div class="display-pokemon">
+          <img class="pokemon-img" src="./imgs/charizard.gif" alt="Charizard" />
+          <br/> 
+          <br/>
+            <h3 class="n-degree">N° 6</h3>
+          <h2 class="pokemon-name">Charizard</h2>
+          <br/>
+          <span class="abilities">
+                <span class="type-ability" style="background-color: #85aeff">
+                  Flying
+                </span>
+                <span class="type-ability" style="background-color: #ff6b52">
+                  Fire
+                </span>
+          </span>
+          <p>Spits fire that is hot enough to melt boulders. known to cause forest fires unintentionally.</p>
+            <div class="width-height-container">
+            <div class="width">
+            <h2>Width</h2>
+            </div>
+
+             <div class="height">
+            <h2>Height</h2>
+            </div>
+          </div>
+          </div>
+    `;
+  }
+}
+
 // Dark theme preference
 let psyduck = document.querySelector("#psyduck");
 psyduck.addEventListener("click", displayTheme);
@@ -59,3 +96,6 @@ window.addEventListener("load", () => {
 
 let pixelPikachu = document.querySelector("#pixel-pikachu");
 pixelPikachu.addEventListener("click", pikaPika);
+
+let pokemonElement = document.querySelector("#pokemon");
+pokemonElement.addEventListener("click", displayPokemon);
